518fb8c8ee2aae906f4f1a0ee9b98e8cb0048e42
bsvega <bsvega@bancolombia.com.co>
2023-12-23 11:17:05 -0500

    add save method on FileUseCase

--- domain/usecase/src/main/java/co/com/ufps/usecase/file/FileUseCase.java
+++ domain/usecase/src/main/java/co/com/ufps/usecase/file/FileUseCase.java
@@ -1,16 +1,23 @@
 package co.com.ufps.usecase.file;
 
+import co.com.ufps.model.file.gateways.FileReaderRepository;
 import co.com.ufps.model.file.gateways.FileRepository;
 import lombok.RequiredArgsConstructor;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
 public class FileUseCase {
+    private final FileReaderRepository fileReaderRepository;
     private final FileRepository fileRepository;
 
     public List<Map<String, Object>> read(Object file) {
-        return fileRepository.read(file);
+        return fileReaderRepository.read(file);
+    }
+
+    public void save(String name, File file) {
+        fileRepository.save(name, file);
     }
 }
