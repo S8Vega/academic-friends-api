84225e375de8debbf3ac44af0196c741fb81ab4b
bsvega <bsvega@bancolombia.com.co>
2024-01-06 15:50:10 -0500

    change s3 config

--- infrastructure/driven-adapters/s3-repository/src/main/java/co/com/ufps/s3/config/S3Config.java
+++ infrastructure/driven-adapters/s3-repository/src/main/java/co/com/ufps/s3/config/S3Config.java
@@ -3,7 +3,7 @@ package co.com.ufps.s3.config;
 import co.com.ufps.s3.config.model.S3ConnectionProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -14,7 +14,7 @@ public class S3Config {
     public S3Client localS3Client(S3ConnectionProperties s3Properties) {
         return S3Client.builder()
                 .region(Region.of(s3Properties.getRegion()))
-                .credentialsProvider(ProfileCredentialsProvider.create("default"))
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
 }
