9e7eb2198868ac9ea75d56354620c62b292dfe13
bsvega <bsvega@bancolombia.com.co>
2024-01-07 13:55:18 -0500

    add environment variables on S3Config

--- infrastructure/driven-adapters/s3-repository/src/main/java/co/com/ufps/s3/config/S3Config.java
+++ infrastructure/driven-adapters/s3-repository/src/main/java/co/com/ufps/s3/config/S3Config.java
@@ -1,20 +1,30 @@
 package co.com.ufps.s3.config;
 
 import co.com.ufps.s3.config.model.S3ConnectionProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
+    @Value("${aws_access_key_id}")
+    private String awsAccessKeyId;
+    @Value("${aws_secret_access_key}")
+    private String awsSecretAccessKey;
+
+    public AwsBasicCredentials awsCredentials() {
+        return AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey);
+    }
 
     @Bean
     public S3Client localS3Client(S3ConnectionProperties s3Properties) {
         return S3Client.builder()
                 .region(Region.of(s3Properties.getRegion()))
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials()))
                 .build();
     }
 }
